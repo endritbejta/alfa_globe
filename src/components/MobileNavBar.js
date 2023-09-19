06bf0fbbede4e59177c795291f52d972e5bce946
endritbejta <endrit.bejta@hotmail.com>
2023-09-19 13:07:05 +0200

    home bug

--- src/components/MobileNavBar.js
+++ src/components/MobileNavBar.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "./MobileNavBar.scss";
+import { NavLink } from "react-router-dom";
 
 const MobileNavBar = ({ setShowOptions, showOptions }) => {
   return (
     <div className="mobile-nav">
-      <a href="#home" className="mobile-nav__item">
+      <NavLink href="#home" className="mobile-nav__item">
         <i className="fa-solid fa-house"></i>
-      </a>
-      <a href="#about" className="mobile-nav__item">
+      </NavLink>
+      <NavLink href="#about" className="mobile-nav__item">
         <i className="fa-solid fa-tractor"></i>
-      </a>
-      <a href="#about" className="mobile-nav__item">
+      </NavLink>
+      <NavLink href="#about" className="mobile-nav__item">
         <i className="fa-solid fa-gas-pump"></i>
-      </a>
-      <a href="#contact" className="mobile-nav__item">
+      </NavLink>
+      <NavLink href="#contact" className="mobile-nav__item">
         <i className="fa-regular fa-building"></i>
-      </a>
+      </NavLink>
       <div
         onClick={() => setShowOptions(!showOptions)}
         className="mobile-nav__item mobile-nav__item--menu"

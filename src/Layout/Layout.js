4d4a07a6e75a058eb07c942bdfb12fec52d7035d
endritbejta <endrit.bejta@hotmail.com>
2023-09-19 13:12:14 +0200

    another bug

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import MobileNavBar from "../components/MobileNavBar";
 import { Outlet } from "react-router";
-
+import "./Layout.scss";
 const Layout = () => {
   const [showOptions, setShowOptions] = useState(false);
 

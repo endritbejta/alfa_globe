9a60411172d74b8c7f20613068e868ece628b7e0
endritbejta <endrit.bejta@hotmail.com>
2023-09-19 19:39:25 +0200

    fix/ navlink wrong onClick Handler

--- src/components/MobileNavBar.js
+++ src/components/MobileNavBar.js
@@ -20,34 +20,44 @@ const MobileNavBar = ({ setShowOptions, showOptions }) => {
     document.body.classList.remove("hidden");
   };
 
+  const handleNavigationClicks = (e) => {
+    e.target.classList.contains("mobile-nav__item--menu") &&
+      handleShowNavigation();
+    e.target.classList.contains("navigation_item") && handleNavClicks();
+  };
+
   return (
-    <div className="mobile-nav">
-      <NavLink to="" onClick={handleNavClicks} className="mobile-nav__item">
+    <div className="mobile-nav" onClick={(e) => handleNavigationClicks(e)}>
+      <NavLink
+        to=""
+        // onClick={handleNavClicks}
+        className="mobile-nav__item navigation_item"
+      >
         <i className="fa-solid fa-house"></i>
       </NavLink>
       <NavLink
         to="agriculture"
-        onClick={() => setShowOptions((showOptions) => !showOptions)}
-        className="mobile-nav__item"
+        // onClick={handleNavClicks}
+        className="mobile-nav__item navigation_item"
       >
         <i className="fa-solid fa-tractor"></i>
       </NavLink>
       <NavLink
         to="derivate"
-        onClick={handleNavClicks}
-        className="mobile-nav__item"
+        // onClick={handleNavClicks}
+        className="mobile-nav__item navigation_item"
       >
         <i className="fa-solid fa-gas-pump"></i>
       </NavLink>
       <NavLink
         to="construction"
-        onClick={handleNavClicks}
-        className="mobile-nav__item"
+        // onClick={handleNavClicks}
+        className="mobile-nav__item navigation_item"
       >
         <i className="fa-regular fa-building"></i>
       </NavLink>
       <div
-        onClick={handleShowNavigation}
+        // onClick={handleShowNavigation}
         className="mobile-nav__item mobile-nav__item--menu"
       >
         <i className="fa-solid fa-bars"></i>

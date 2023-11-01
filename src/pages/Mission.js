29711b2aed6183f3c50d0b2198dbf3fb55a11dbb
endritbejta <endrit.bejta@hotmail.com>
2023-11-01 13:38:42 +0100

    no message

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 import "./Mission.scss";
 import logo from "../assets/image/highway2.jpg";
+import { VscDebugBreakpointLog } from "react-icons/vsc";
 
 const Mission = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <div className="pagemission-page page">
+    <div className="pagemission page">
       <section className="pagemission-intro">
         <div className="pagemission-content">
           <h1>Mission</h1>
@@ -22,37 +23,46 @@ const Mission = () => {
             building future leaders through education and sports.
           </p>
         </div>
-        <img src={logo} alt="alfa trade" />
       </section>
-      <section className="pagemission-list">
-        <ul>
-          <li>
-            Prioritizing the well-being of people and the environment in every
-            action we take.
-          </li>
-          <li>
-            Motivating our employees to work together andcontinuously improve
-            performance.
-          </li>
-          <li>
-            Understanding and meeting the expectations and needs of our
-            customers.
-          </li>
-          <li>Employing effective processes and technologies.</li>
-          <li>
-            Being good corporate citizens, offering employment opportunities,
-            and supporting the community in which we live and operate.
-          </li>
-          <li>
-            Striving to uphold the highest standards of environmental and social
-            responsibility.
-          </li>
-          <li>
-            Embracing innovation and sustainable practices in all aspects of our
-            business.
-          </li>
-        </ul>
-        <img src={logo} alt="alfa trade" />
+      <img src={logo} alt="alfa trade" />
+      <img src={logo} alt="alfa trade" />
+      <section className="pagemission-target">
+        <div className="pagemission-list">
+          <h3>Targets</h3>
+          <ul>
+            <li>
+              <VscDebugBreakpointLog />
+              Prioritizing the well-being of people and the environment in every
+              action we take.
+            </li>
+            <li>
+              <VscDebugBreakpointLog />
+              Motivating our employees to work together andcontinuously improve
+              performance.
+            </li>
+            <li>
+              <VscDebugBreakpointLog />
+              Understanding and meeting the expectations and needs of our
+              customers.
+            </li>
+            <li>Employing effective processes and technologies.</li>
+            <li>
+              <VscDebugBreakpointLog />
+              Being good corporate citizens, offering employment opportunities,
+              and supporting the community in which we live and operate.
+            </li>
+            <li>
+              <VscDebugBreakpointLog />
+              Striving to uphold the highest standards of environmental and
+              social responsibility.
+            </li>
+            <li>
+              <VscDebugBreakpointLog />
+              Embracing innovation and sustainable practices in all aspects of
+              our business.
+            </li>
+          </ul>
+        </div>
       </section>
     </div>
   );

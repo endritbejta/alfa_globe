017deb95733d6fc8582bd2b35f1d232657d5c75f
endritbejta <endrit.bejta@hotmail.com>
2023-11-01 09:21:45 +0100

    no message

--- src/pages/Construction.js
+++ src/pages/Construction.js
@@ -5,7 +5,14 @@ const Construction = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  return <div className="construction-page page">Construction</div>;
+  return (
+    <div className="construction-page page">
+      <h1>Build with us</h1>
+      <section className="contruction-intro"></section>
+      <section className="contruction-content"></section>
+      <section className="contruction-outro"></section>
+    </div>
+  );
 };
 
 export default Construction;
